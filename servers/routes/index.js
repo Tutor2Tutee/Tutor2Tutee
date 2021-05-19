92bfe93515c4220f682054f4084cc51024cd5019
Hong Jinwoo <jinwoo17962@gmail.com>
2021-05-20 00:04:16 +0900

    added : jwt-token implementation

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -4,8 +4,24 @@ const router = express.Router();
 const _class = require('./Class')
 const user = require('./User')
 
+// 권한이 필요한곳에 적용!
+const authMiddleWare = require('../middleware/auth')
+
 router.use('/class',_class)
 router.use('/user', user)
+
+
+
+// /secret 에 get을 하려면 token이 필요함!
+/*
+router.use('/secret', authMiddleWare)
+router.get('/secret', (req,res)=> {
+    res.status(200).json({
+        success : true,
+        message : "success"
+    })
+})
+ */
 
 
 
