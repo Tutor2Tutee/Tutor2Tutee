22893832e88f7af83630ec1219f99024b8268eed
Hong Jinwoo <jinwoo17962@gmail.com>
2021-05-20 00:26:09 +0900

    changed : route to login and register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 export {default as Home} from './Home'
 export {default as About} from './About'
 export {default as Class} from './Class'
-export {default as Login} from './Login'
-export {default as Register} from './Register'
+export {default as Login} from '../components/User/Login'
+export {default as Register} from '../components/User/Register'
