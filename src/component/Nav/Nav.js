0e967bacf23afe7a6c2ff860a8f1f057d403c3dd
Hong Jinwoo <jinwoo17962@icloud.com>
2021-06-10 23:20:37 +0900

    Update Nav.js

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -11,13 +11,13 @@ export default function Nav(props){
     return (
         <nav className="header__nav">
             <ul className="header__ul">
-                <NavLink href="/" >{language === "kor" ? "홈페이지" : "Home"}</NavLink>
-                <NavLink href="/feature" >{language === "kor" ? "특색" :"Feature"}</NavLink>
-                <NavLink href="/about" >{language === "kor" ? "약" :"About"}</NavLink>
+                <NavLink href="/" >{language === "kor" ? "홈" : "Home"}</NavLink>
+                <NavLink href="/feature" >{language === "kor" ? "기능" :"Feature"}</NavLink>
+                <NavLink href="/about" >{language === "kor" ? "소개" :"About"}</NavLink>
                 {
                     isAuth && (<React.Fragment> 
                     <NavLink href="/class" >{language === "kor" ? "클래스" : "Classes"}</NavLink>
-                    <NavLink href="/logout" >{language === "kor" ? "로그 아웃" : "Logout"}</NavLink>
+                    <NavLink href="/logout" >{language === "kor" ? "로그아웃" : "Logout"}</NavLink>
                     </React.Fragment>
                     )
                 }
@@ -25,7 +25,7 @@ export default function Nav(props){
                     !isAuth && (
                     <React.Fragment>
                     <NavLink href="/login" >{language === "kor" ? "로그인" : "Login"}</NavLink>
-                    <NavLink href="/signup" >{language === "kor" ? "레지스터" : "Signup"}</NavLink>
+                    <NavLink href="/signup" >{language === "kor" ? "회원가입" : "Signup"}</NavLink>
                     </React.Fragment>
                     )
                 }
