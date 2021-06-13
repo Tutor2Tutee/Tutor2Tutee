3654c2415f1f2b8ba4bb7ab39da1beb1fe65d456
Nikhil <nikhilsharmamusic2000@gmail.com>
2021-06-13 20:04:36 +0530

    [Site] Added language.json locale file

--- src/pages/Feature/Feature.js
+++ src/pages/Feature/Feature.js
@@ -3,6 +3,8 @@ import "./Feature.css"
 import ImgSrc from './../../shared/ImgSrc'
 import { useSelector } from 'react-redux'
 
+import Locale from './../../locale/language.json'
+
 const Feature = () => {
     let language = useSelector(state => state.lang.language)
 
@@ -11,8 +13,8 @@ const Feature = () => {
             <div className="feature__main">
                 <div className="feature__info--container">
                     <div className="feature__info">
-                        <h1 className="feature__h1">{language === "kor" ? "비디오 강의" :"Recorded Video"}</h1>
-                        <p className="feature__p">{language === "kor" ? "여가 시간에 녹화 된 비디오를 활용하여 새로운 것을 배우세요!" : "Utilize the recorded video in your free time to learn Something New"}</p>
+                        <h1 className="feature__h1">{Locale.feature.feature_recorded_head[language]}</h1>
+                        <p className="feature__p">{Locale.feature.feature_recorded_para[language]}</p>
                     </div>
                 </div>
                 <div className="feature__banner">
@@ -25,16 +27,16 @@ const Feature = () => {
                 </div>
                 <div className="feature__info--container">
                     <div className="feature__info feature__info--paddingleft">
-                        <h1 className="feature__h1">{language === "kor" ? "온라인 및 오프라인 회의" :"Online & Offline Meeting"}</h1>
-                        <p className="feature__p">{language === "kor" ? "Zoom 또는 Skype에서 오프라인 및 온라인 회의 실시간 회의를 할수 있어요!" : "Offline & Online meeting realtime meeting on Zoom or Skype"}</p>
+                        <h1 className="feature__h1">{Locale.feature.feature_meeting_head[language]}</h1>
+                        <p className="feature__p">{Locale.feature.feature_meeting_para[language]}</p>
                     </div>
                 </div>
             </div>
             <div className="feature__main">
                 <div className="feature__info--container">
                     <div className="feature__info">
-                        <h1 className="feature__h1">{language === "kor" ? "출석 체크" : "Attandance Checking"}</h1>
-                        <p className="feature__p">{language === "kor" ? "튜티들은 출석체크를 통해 평가를 받을 수 있어요!" : "Attendance Checking To Evaluate Tutee"}</p>
+                        <h1 className="feature__h1">{Locale.feature.feature_attandance_head[language]}</h1>
+                        <p className="feature__p">{Locale.feature.feature_attandance_para[language]}</p>
                     </div>
                 </div>
                 <div className="feature__banner">
@@ -47,8 +49,8 @@ const Feature = () => {
                 </div>
                 <div className="feature__info--container">
                     <div className="feature__info feature__info--paddingleft">
-                        <h1 className="feature__h1">{language === "kor" ? "평가" : "Rating"}</h1>
-                        <p className="feature__p">{language === "kor" ? "튜티는 강의질을 향상 시키기 위해 튜터를 평가 할수있어요!" : "Tutor Rating System, Used By Tutee To Improve Class Quality"}</p>
+                        <h1 className="feature__h1">{Locale.feature.feature_rating_head[language]}</h1>
+                        <p className="feature__p">{Locale.feature.feature_rating_para[language]}</p>
                     </div>
                 </div>
             </div>
