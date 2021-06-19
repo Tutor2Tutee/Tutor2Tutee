80585857d51454c409f591a8c817ee3ec10faa2a
Kartik <kartikth40@gmail.com>
2021-06-19 14:47:49 +0530

    added dark mode button to the site

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,49 +1,66 @@
-import React,{useState} from 'react'
-import "./Header.css"
-import { useSelector,useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import './Header.css'
+import { useSelector, useDispatch } from 'react-redux'
 
 import ImgSrc from './../../shared/ImgSrc'
 import Nav from './../Nav/Nav'
 import * as actionCreator from './../../store/actions/language'
+import toggleTheme from './../../store/actions/theme'
 
 const Header = (props) => {
-    const [showLang,setShowLang] = useState(false)
+  const [showLang, setShowLang] = useState(false)
 
-    let language = useSelector(state => state.lang.language)
-    
-    const dispatch = useDispatch()
-    const languageHandler = (lang) => {
-        dispatch(actionCreator.toggleLang(lang))
-    }
+  let language = useSelector((state) => state.lang.language)
+  let isDark = useSelector((state) => state.theme.isDark)
+  console.log(isDark)
+  const dispatch = useDispatch()
 
-    return (
-        <header className="header">
-            <div><h1 className="header__h1">Tutor<label className="header__h1--label">2</label>Tutee</h1></div>
-            <div className="header__lang--container">
-                <div className="header__lang--main" onClick={() => setShowLang(prev => !prev)} > 
-                    <div>
-                        <img src={ImgSrc.translate} /> 
-                    </div>
-                    <h1>{language === "kor" ? "언어" : "Language"}</h1>
-                </div>
-                {
-                    showLang && (
-                    <div className="header__lang--option" onClick={() => setShowLang( prev => !prev)}>
-                        <button onClick={languageHandler.bind(this,"kor")}>한국어</button>
-                        <button onClick={languageHandler.bind(this,"en")}>English</button>
-                    </div>
-                    )
-                }
-            </div>
-            
-            <div onClick={props.toggleSD} className="header_toggle--btn">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <Nav />
-        </header>
-    )
+  const languageHandler = (lang) => {
+    dispatch(actionCreator.toggleLang(lang))
+  }
+  const themeButtonHandler = () => {
+    dispatch(toggleTheme())
+  }
+
+  return (
+    <header className={`header ${!isDark && 'light'}`}>
+      <div>
+        <h1 className={`header__h1 ${!isDark && 'light'}`}>
+          Tutor<label className={`header__h1--label ${!isDark && 'light'}`}>2</label>Tutee
+        </h1>
+      </div>
+      <div className={`header__lang--container ${!isDark && 'light'}`}>
+        <div className="header__lang--main" onClick={() => setShowLang((prev) => !prev)}>
+          <div>
+            <img src={ImgSrc.translate} />
+          </div>
+          <h1>{language === 'kor' ? '언어' : 'Language'}</h1>
+        </div>
+        {showLang && (
+          <div className={`header__lang--option ${!isDark && 'light'}`} onClick={() => setShowLang((prev) => !prev)}>
+            <button className={`${!isDark && 'light'}`} onClick={languageHandler.bind(this, 'kor')}>
+              한국어
+            </button>
+            <button className={`${!isDark && 'light'}`} onClick={languageHandler.bind(this, 'en')}>
+              English
+            </button>
+          </div>
+        )}
+      </div>
+      {/* Theme toggler */}
+      <div className="header__theme--container">
+        <button onClick={() => themeButtonHandler(isDark)} className="header__theme--button">
+          toggle
+        </button>
+      </div>
+      <div onClick={props.toggleSD} className="header_toggle--btn">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <Nav />
+    </header>
+  )
 }
 
-export default Header;
+export default Header
--- src/component/UI/NavLink/NavLinks.js
+++ src/component/UI/NavLink/NavLinks.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import './NavLinks.css'
 
 export default function NavLinks(props) {
+  let isDark = useSelector((state) => state.theme.isDark)
   return (
     <li className="header__nav--li">
-      <NavLink activeStyle={{ color: 'var(--light-primary-purple)' }} to={`${props.href}`} exact={true}>
+      <NavLink className={`nav-link ${!isDark && 'light'}`} to={`${props.href}`} exact={true}>
         {props.children}
       </NavLink>
     </li>
