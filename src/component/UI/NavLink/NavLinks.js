58ed100a0b9b5ffd14d4d40fb45675b85d9f1676
Kartik <kartikth40@gmail.com>
2021-06-19 12:10:38 +0530

    converted the site into full dark mode

--- src/component/UI/NavLink/NavLinks.js
+++ src/component/UI/NavLink/NavLinks.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import "./NavLinks.css"
+import './NavLinks.css'
 
-export default function NavLinks(props){
-    return (
-        <li className="header__nav--li"><NavLink activeStyle={{color: props.sd ? "var(--light-primary-purple)" : "var(--light-primary-purple-dark)"}} to={`${props.href}`} exact={true}>{props.children}</NavLink></li>
-    )
+export default function NavLinks(props) {
+  return (
+    <li className="header__nav--li">
+      <NavLink activeStyle={{ color: 'var(--light-primary-purple)' }} to={`${props.href}`} exact={true}>
+        {props.children}
+      </NavLink>
+    </li>
+  )
 }
