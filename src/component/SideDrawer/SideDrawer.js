fe009738eb3defdf90024679340ce86af8ff5a4c
Nikhil <nikhilsharmamusic2000@gmail.com>
2021-06-22 20:32:53 +0530

    [Site] Fixed Night Mode Button Responsiveness

--- src/component/SideDrawer/SideDrawer.js
+++ src/component/SideDrawer/SideDrawer.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './SideDrawer.css'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 
 import NavLink from './../UI/NavLink/NavLinks'
+import toggleTheme from './../../store/actions/theme'
 
 
 const SideDrawer = (props) => {
     let isAuth = useSelector(state => state.auth.isAuthenticated)
     let language = useSelector(state => state.lang.language)
+
+    let isDark = useSelector((state) => state.theme.isDark)
+
+    const dispatch = useDispatch()
+
+    const handleThemeBtn = () => {
+        dispatch(toggleTheme())
+      }
 
     const Drawer = (
         <nav style={{transform:props.showSD ? 'translateX(0)':'translateX(-120%)'}} className="sidedrawer__container">
@@ -32,6 +41,15 @@ const SideDrawer = (props) => {
                     )
                 }
             </ul>
+            {/* Theme toggler */}
+            <div className="sd-theme-btn-container">
+                <div onClick={handleThemeBtn} className={`sd-theme-btn ${!isDark ? 'light' : 'dark'}`}>
+                    <button className={`sd-slider ${!isDark ? 'light' : 'dark'}`}></button>
+                    <h1 className={!isDark && 'light'}>
+                    {isDark ? (language === 'kor' ? '어두운' : 'Dark') : language == 'kor' ? '빛' : 'Light'}
+                    </h1>
+                </div>
+            </div>
         </nav>
     )
 
