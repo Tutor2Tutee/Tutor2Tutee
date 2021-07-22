52d14dccf88a124596dedc77e660996831d31419
jinwoo1225 <jinwoo17962@gmail.com>
2021-07-22 19:23:27 +0900

    added : whoListened to exact who listened :)

--- servers/models/classtypes/recordedVideoSchema.js
+++ servers/models/classtypes/recordedVideoSchema.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose');
+const User = require('../userSchema');
 
-
+// TODO : Attendance system
+// if user access to this video
+// user will get
 const videoSchema = new mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required: true, unique: true},
     created: {type: Date, default: Date.now},
-    linkToVideo : {type: String, required: true}
+    linkToVideo: {type: String, required: true},
+    whoListened: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}]
 })
 
 const recordedVideoSchema = new mongoose.Schema({
-    classes: [{type: videoSchema}],
-    // whoListened : []
+    classes: [{type: videoSchema, ref:"Video"}],
 })
+
+mongoose.model("Video", videoSchema)
+
+
+// TODO : video should append by method in recordedVideoSchema's array
+// ADD APPEND METHOD HERE
+videoSchema.methods.checkAttendance = async function (attendee) {
+    await this.updateOne
+}
 
 
 module.exports = mongoose.model('RecordedVideo', recordedVideoSchema);
