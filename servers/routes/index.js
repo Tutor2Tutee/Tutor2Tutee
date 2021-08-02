e1291ea33c1e6d2537d1472106951024503e433e
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-08-02 20:07:45 +0530

    Added Quiz to API

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const _class = require('./Class')
 const user = require('./User')
+const _quiz = require('./Quiz')
 
 router.use('/class', _class)
 router.use('/user', user)
+router.use('/quiz', _quiz)
 
 
 module.exports = router
