afe30caf8de654858232a6be284fd7b27bf503f2
Hong Jinwoo <jinwoo17962@icloud.com>
2021-08-18 22:56:14 +0900

    added : bcrypt support

--- servers/api/auth/auth.service.ts
+++ servers/api/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { User, UserDocument } from '../users/schemas/user.schema';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,8 +14,7 @@ export class AuthService {
 
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.userModel.findOne({ email }).exec();
-
-        if (user && user.password === password) {
+        if (user && (await bcrypt.compare(password, user.password))) {
             return { _id: user._id, email: user.email };
         }
 
