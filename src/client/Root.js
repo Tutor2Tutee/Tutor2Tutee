cf859bca90e529d25f299376090d48f58ae0de13
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-08-25 16:34:53 +0530

    cleanup

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import {BrowserRouter} from "react-router-dom";
-import App from '../shared/App';
-import AppLatest from '../shared/App_Latest';
-import "./Root.css"
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import App from "../shared/App";
+import "./Root.css";
 
 const Root = () => {
-    // 추후에 리덕스는 여기에 적용하기!
-    return <>
-        <BrowserRouter>
-            <AppLatest/>
-        </BrowserRouter>
+  // 추후에 리덕스는 여기에 적용하기!
+  return (
+    <>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </>
+  );
 };
 
-export default Root
+export default Root;
--- src/routes/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LoginPage from '../components/User/Login'
-
-const Login = () => {
-    return <div>
-        <LoginPage/>
-    </div>
-}
-export default Login
