c5e7e9b149da038a6328358d991ad72ab630132a
Hong Jinwoo <jinwoo17962@icloud.com>
2021-09-01 00:25:43 +0900

    added : logging middleware

--- servers/app.module.ts
+++ servers/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './api/common/middlewares/logger.middleware';
 
 const dbOpt = {
     useNewUrlParser: true,
@@ -23,4 +24,8 @@ const dbOpt = {
     controllers: [],
     providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
