c67941fc76b54f142153fa99b05a06dfeeea642e
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-09-09 16:03:42 +0530

    [FrontEnd] Default language to English

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-import "./NotFound.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
-  return (
-    <section className="notfound_container">
-      <section class="error-container">
-        <span>4</span>
-        <span><span class="screen-reader-text">0</span></span>
-        <span>4</span>
-      </section>
-      <p className="notfound_para">Sorry, this page isn't available</p>
-      <div class="link-container">
-        <Link to="/dashboard" class="more-link">Go To Dashboard</Link>
-      </div>
-    </section>
-  );
+    return (
+        <section className="notfound_container">
+            <section class="error-container">
+                <span>4</span>
+                <span>
+                    <span class="screen-reader-text">0</span>
+                </span>
+                <span>4</span>
+            </section>
+            <p className="notfound_para">Sorry, this page isn't available</p>
+            <div class="link-container">
+                <Link to="/dashboard" class="more-link">
+                    Go To Dashboard
+                </Link>
+            </div>
+        </section>
+    );
 };
 
 export default NotFound;
